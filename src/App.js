0c21dfff21bca8b574179c10ad657de5321d2645
adkhiker <poirier.greg@gmail.com>
2019-10-11 13:34:21 -0400

    Fragments

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React from "react";
 import "./App.scss";
-import NameTag from './components/nameTag';
-
-
+import NameTag from "./components/nameTag";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1 className="name title">Names List</h1>
-        <NameTag name="Peter">Peter</NameTag>
-        <NameTag name="Paul">Paul</NameTag>
-        <NameTag name="Mary">Mary</NameTag>
+        <NameTag firstName="Peter" lastName="Peterson"></NameTag>
+        <NameTag firstName="John" lastName="Johnson"></NameTag>
+        <NameTag firstName="Mary" lastName="Maryson"></NameTag>
       </header>
     </div>
   );
